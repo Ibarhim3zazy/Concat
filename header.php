c2cd43071270c7609f17f048a7ded75ea5031913
nello8-0 <nour20900@gmail.com>
2022-03-09 19:47:48 +0200

    header just html

--- header.php
+++ header.php
@@ -5,11 +5,63 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="icon" href="images/bear.svg">
+    <link rel="stylesheet" href="css/header.css">
     <title>Concat</title>
 </head>
 
 <body>
-
+    <div class="header" id="header">
+        <div class="container">
+            <div class="left">
+                <div class="logo">
+                    <a href=""><img src="images/bear.svg" alt=""></a>
+                </div>
+                <div class="search">
+                    <input type="search" name="" id="search">
+                    <a href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+                </div>
+            </div>
+            <div class="middle">
+                <div class="home">
+                    <a href=""><i class="fa-solid fa-house-user"></i></a>
+                    <span><a href="">Home</a></span>
+                </div>
+                <div class="group">
+                    <a href=""><i class="fa-solid fa-users"></i></a>
+                    <span>
+                        <a href="">Groups</a>
+                    </span>
+                </div>
+                <div class="courses">
+                    <a href=""><i class="fa-solid fa-book"></i></a>
+                    <span>
+                        <a href="">Courses</a>
+                    </span>
+                </div>
+            </div>
+            <div class="right">
+                <div class="account">
+                    <a href=""><img src="images/cat-1.jpg" alt=""></a>
+                    <span>
+                        <a href="">User Name</a>
+                    </span>
+                </div>
+                <div class="message">
+                    <a href=""><i class="fa-brands fa-facebook-messenger"></i></a>
+                </div>
+                <div class="notification">
+                    <a href=""><i class="fa-solid fa-bell"></i></a>
+                </div>
+                <div class="more">
+                    <a href=""><i class="fa-solid fa-bars"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
