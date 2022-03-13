f4e994802b33ed1d24ab9a903899f584748832be
nello8-0 <nour20900@gmail.com>
2022-03-13 21:25:38 +0200

    edit post

--- js/post.js
+++ js/post.js
@@ -1,3 +1,27 @@
+// const container = document.querySelector(".wrapper")
+// const privacy = container.querySelector(".privacy")
+
+// const  arrowBack = container.querySelector(".arrow-back");
+
+// privacy.addEventListener("click", () => {
+//     container.classList.add("active")
+// });
+// arrowBack.addEventListener("click", () => {
+//         container.classList.remove("active")
+//     });
+
+// const pup = document.querySelector(".pop_post");
+
+// pup.addEventListener("click", () => {
+//     // pup.style.display = 'none';
+// })
+// function showPost() {
+//     pup.style.display = 'block';
+// }
+
+
+// edit 
+
 const container = document.querySelector(".wrapper")
 const privacy = container.querySelector(".privacy")
 
@@ -5,20 +29,10 @@ const  arrowBack = container.querySelector(".arrow-back");
 
 privacy.addEventListener("click", () => {
     container.classList.add("active")
-});
+}); 
 arrowBack.addEventListener("click", () => {
         container.classList.remove("active")
-    });
-
-const pup = document.querySelector(".pop_post");
-
-pup.addEventListener("click", () => {
-    // pup.style.display = 'none';
-})
-function showPost() {
-    pup.style.display = 'block';
-}
-
+    }); 
 
 
 // change pop up  start 
