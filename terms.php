cfef9abdbc79e78fc30b8fa506cfd27d065730a2
Manal M Eisa <eisa48028@gmail.com>
2022-03-15 23:44:45 +0200

    init

---
+++ terms.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terms and conditions</title>
+    <link rel="stylesheet" href="css/terms.css">
+    <link rel="stylesheet" href="css/general.css">
+</head>
+<body>
+  <div class="modal-content">
+   
+         
+          <h2 class="modal-title" id="policyModalLabel">Privacy and data security policy</h2>
+   
+    
+    
+    <div class="modal-body">
+      <ul>
+          <li>All data that is recorded on the system is owned by the owner of this data</li>
+          <li>These data are not viewed by the center’s engineers except for the utmost necessity and for the purpose of technical support and in agreement with the party that owns the data, orally or in writing</li>
+          <li>No statement from the system data is given to any individual or sub-party belonging to the data-author or any other entity without the written consent of the data-owner</li>
+          <li>The data recorded on the system can be used in order to create statistics that are useful in measuring the performance of the system and the efficiency of its use by its users</li>
+          <li>All system data is secured by securing the server computer that hosts the data and by securing the transmission of data between the user’s device and the server computer, as well as securing the system interface used by the user</li>
+          <li>Each user of the system is fully responsible for his password and must take all possible precautions so that no one else can obtain it</li>
+      </ul>
+    </div>
+   
+</div>
+ <img src="./images/cyber-security-vector-illustration-concept-characters-data-protected-access-control-privacy-protection-modern-flat-style-190881812.jpg">
+  
+</body>
+</html>
