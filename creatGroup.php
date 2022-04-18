1377626770d2438da52fee08923f77ad049326ad
nello8-0 <nour20900@gmail.com>
2022-04-18 02:49:52 +0200

    group

---
+++ creatGroup.php
@@ -0,0 +1,18 @@
+<?php require_once 'mail_check_ajax.php';
+ require_once 'header.php';
+     ?>
+
+<div class="form create_group">
+ <div class="container">
+  <p><a href="group.php">Groups</a> > Create group</p>
+  <h2>Create group</h2>
+  <div class="admin">
+   <img src="user_media/image/bear.jpg" alt="" width="40px" height="40px" style="border-radius: 50%;">
+   <div class="admin_name">
+    <h3>User name</h3>
+    <span>Admin</span>
+   </div>
+  </div>
+
+ </div>
+</div>
--- group.php
+++ group.php
@@ -1,16 +1,76 @@
-<!DOCTYPE html>
-<html lang="en">
+<link rel="stylesheet" href="css/group.css">
 
-<head>
- <meta charset="UTF-8">
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
- <title>Concat| Groups </title>
-</head>
+<?php require_once 'mail_check_ajax.php';
+ require_once 'header.php';
+     ?>
 
-<body>
+<div class="home">
+ <div class="container">
+  <div class="left">
+   <div class="contain">
+    <h1>Groups</h1>
+    <a href="creatGroup.php"> <i class="fa-solid fa-plus"></i> Create new group</a>
+    <hr width="100%" size="1" color="#eee">
+    <div class="manager">
+     <h2>Groups you've managed</h2>
+     <div class="box">
+      <a href="" style="width: 100%;">
+       <div class="namegroup">
+        <img src="user_media/image/bear.jpg" alt="" width="100px" height="100px">
+        <h3>Aesthetics Art Aest Ar sqas</h3>
+       </div>
+       <i class="fa-solid fa-thumbtack"></i>
+      </a>
+      <a href="">
+       <img src="user_media/image/bear.jpg" alt="" width="100px" height="100px">
+       <h3>Aesthetics Art</h3>
+      </a>
+      <a href="">
+       <img src="user_media/image/bear.jpg" alt="" width="100px" height="100px">
+       <h3>Aesthetics Art</h3>
+      </a>
+      <a href="">
+       <img src="user_media/image/bear.jpg" alt="" width="100px" height="100px">
+       <h3>Aesthetics Art</h3>
+      </a>
+     </div>
+    </div>
+    <hr width="100%" size="1" color="#eee">
+    <div class="manager">
+     <h2>Groups you've Joined</h2>
+     <div class="box">
+      <a href="">
+       <div class="namegroup">
+        <img src="user_media/image/bear.jpg" alt="" width="100px" height="100px">
+        <h3>Aesthetics Art</h3>
+       </div>
+       <i class="fa-solid fa-thumbtack"></i>
+      </a>
+      <a href="">
+       <img src="user_media/image/bear.jpg" alt="" width="100px" height="100px">
+       <h3>Aesthetics Art</h3>
+      </a>
+      <a href="">
+       <img src="user_media/image/bear.jpg" alt="" width="100px" height="100px">
+       <h3>Aesthetics Art</h3>
+      </a>
+      <a href="">
+       <img src="user_media/image/bear.jpg" alt="" width="100px" height="100px">
+       <h3>Aesthetics Art</h3>
+      </a>
+     </div>
+    </div>
+   </div>
+  </div>
+  <div class="middle">
+   <?php require_once 'groupPosts.php'
+     ?>
+  </div>
+  <div class="right">
+   <div class="contain">
 
-</body>
-
-</html>
+   </div>
+  </div>
+ </div>
+</div>
