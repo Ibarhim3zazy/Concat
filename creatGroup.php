421a36cbb5bcf642346eef02e711ddbfde7d7fc0
nello8-0 <nour20900@gmail.com>
2022-04-18 17:19:28 +0200

    create group

--- creatGroup.php
+++ creatGroup.php
@@ -1,22 +1,36 @@
+<link rel="stylesheet" href="css/creategroup.css">
+
 <?php require_once 'mail_check_ajax.php';
  require_once 'header.php';
      ?>
 
 <div class="form create_group">
  <div class="container">
-  <p><a href="group.php">Groups</a> > Create group</p>
-  <h2>Create group</h2>
-  <div class="admin">
-   <img src="user_media/image/bear.jpg" alt="" width="40px" height="40px" style="border-radius: 50%;">
-   <div class="admin_name">
-    <h3>User name</h3>
-    <span>Admin</span>
+  <div class="contain">
+   <p><a href="group.php">Groups</a> > Create group</p>
+   <h2>Create group</h2>
+   <div class="admin">
+    <img src="user_media/image/bear.jpg" alt="" width="40px" height="40px" style="border-radius: 50%;">
+    <div class="admin_name">
+     <h3>User name</h3>
+     <span>Admin</span>
+    </div>
    </div>
+   <form action="">
+    <div class="top">
+     <input type="text" name="groupName" placeholder="Group Name" required>
+     <div class="select">
+      <label for="privacy">Chosse Privacy</label>
+      <select name="privacy" id="privacy" required>
+       <option value="private">Private</option>
+       <option value="puplic">Puplic</option>
+      </select>
+     </div>
+    </div>
+    <input type="submit" value="Create">
+   </form>
   </div>
-  <input type="text" name="groupName" placeholder="Group Name">
-  <select>
-   <option>Private</option>
-   <option>Personal</option>
-  </select>
+
+  <!-- <input type="button" value="Create"> -->
  </div>
 </div>
