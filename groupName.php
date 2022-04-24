56d2f4d3689576a93ea0cffcab2bc7585bbc504c
nello8-0 <nour20900@gmail.com>
2022-04-24 03:44:10 +0200

    group cover

--- groupName.php
+++ groupName.php
@@ -6,27 +6,28 @@
 
 <header>
  <div class="container">
+  <div class="cover">
+   <img src="user_media/image/cover.jpg" alt="">
+   <p>Group By <a href="somepage.php"> Aesthetics Art</a> </p>
+  </div>
   <div class="contains">
-   <div class="cover">
-    <img src="user_media/image/cover.jpg" alt="">
-    <p>Group By <span>Aesthetics Art</span> </p>
-   </div>
    <div class="info">
     <div class="right">
      <h2>Aesthetics Art</h2>
      <p>
-      <i class="fa-solid fa-earth-africa"></i> Public.
+      <i class="fa-solid fa-earth-africa"></i> Public .
       <a href="member.php" class="people"><span>1.3K</span> members</a>
      </p>
     </div>
     <div class="left">
      <div class="joinbtn">
-      <button><i class="fa-solid fa-users"></i> Joined <i class="fa-solid fa-caret-down"></i></button>
-      <ul>
-       <button><i class="fa-solid fa-thumbtack"></i> Pin Group</button>
-       <button><i class="fa-solid fa-square-plus"></i> Follow Group</button>
-       <button><i class="fa-solid fa-arrow-right-from-bracket"></i> Leave Group</button>
-       <button><i class="fa-solid fa-info"></i> Report Group</button>
+      <button id="joinclk" onclick="clickbtn()" ondblclick="dbclc()"><i class="fa-solid fa-users"></i> Joined <i
+        class="fa-solid fa-caret-down"></i></button>
+      <ul id="btnul">
+       <button><i class="fa-solid fa-thumbtack"></i> Pin</button>
+       <button><i class="fa-solid fa-square-plus"></i> Follow</button>
+       <button><i class="fa-solid fa-arrow-right-from-bracket"></i> Leave</button>
+       <button><i class="fa-solid fa-info"></i> Report</button>
       </ul>
      </div>
      <button>
@@ -37,3 +38,18 @@
   </div>
  </div>
 </header>
+<div class="main">
+ <div class="container">
+  <div class="contains">
+   <?php
+        require 'CreatePost.php';
+      ?>
+   <div class="leftSide">
+    <div class="groupone">
+     <h2>About</h2>
+    </div>
+   </div>
+  </div>
+ </div>
+</div>
+<script src="js/groupname.js"></script>
---
+++ js/groupname.js
@@ -0,0 +1,11 @@
+
+var btnul = document.getElementById('btnul');
+var btn = document.getElementById('joinclk');
+
+function clickbtn() {
+ btnul.style.display = "block";
+}
+
+function dbclc() {
+ btnul.style.display = "none";
+}
