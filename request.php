d2b260a391cfbb3a80bd2bd6ffe1691e67d5d08b
nello8-0 <nour20900@gmail.com>
2022-05-10 13:13:26 +0200

    friend request

--- request.php
+++ request.php
@@ -1,156 +1,139 @@
-<!DOCTYPE html>
-<html lang="en">
+<link rel="stylesheet" href="css/request.css">
 
-<head>
- <meta charset="UTF-8">
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="stylesheet" href="css/all.min.css">
- <link rel="stylesheet" href="css/normalize.css">
- <link rel="stylesheet" href="css/general.css">
- <link rel="icon" href="images/bear.svg">
- <link rel="stylesheet" href="css/request.css">
- <title>Concat | User Name</title>
-</head>
-
-<body>
- <?php require_once 'header.php';
+<?php require_once 'header.php';
     require 'friendsChat.php';
     ?>
- <div class="friend">
-  <div class="container">
-   <div class="title">
-    <div class="con_friend">
-     <p>Friend Requests</p>
-     <div class="search">
-      <i class="fa-solid fa-magnifying-glass"></i>
-      <input type="search" id="search" placeholder="search">
-     </div>
+<div class="friend">
+ <div class="container">
+  <div class="title">
+   <div class="con_friend">
+    <p>Friend Requests</p>
+    <div class="search">
+     <i class="fa-solid fa-magnifying-glass"></i>
+     <input type="search" id="search" placeholder="search">
     </div>
-    <a href="friends.php">Friends</a>
-
    </div>
-   <div class="con_box_friends">
-    <div class="box">
-     <div class="min-box">
-      <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
-      <div class="friendName">
-       <h3>user name</h3>
-       <p><span>5</span>mutual friends</p>
-      </div>
-     </div>
-     <div class="btn">
-      <button><i class="fa-solid fa-user-check"></i></button>
-      <button><i class="fa-solid fa-user-xmark"></i> </button>
+   <a href="friends.php">Friends</a>
+
+  </div>
+  <div class="con_box_friends">
+   <div class="box">
+    <div class="min-box">
+     <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
+     <div class="friendName">
+      <h3>user name</h3>
+      <p><span>5</span>mutual friends</p>
      </div>
     </div>
-    <div class="box">
-     <div class="min-box">
-      <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
-      <div class="friendName">
-       <h3>user name</h3>
-       <p><span>5</span>mutual friends</p>
-      </div>
-     </div>
-     <div class="btn">
-      <button><i class="fa-solid fa-user-check"></i></button>
-      <button><i class="fa-solid fa-user-xmark"></i> </button>
+    <div class="btn">
+     <button><i class="fa-solid fa-user-check"></i></button>
+     <button><i class="fa-solid fa-user-xmark"></i> </button>
+    </div>
+   </div>
+   <div class="box">
+    <div class="min-box">
+     <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
+     <div class="friendName">
+      <h3>user name</h3>
+      <p><span>5</span>mutual friends</p>
      </div>
     </div>
-    <div class="box">
-     <div class="min-box">
-      <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
-      <div class="friendName">
-       <h3>user name</h3>
-       <p><span>5</span>mutual friends</p>
-      </div>
-     </div>
-     <div class="btn">
-      <button><i class="fa-solid fa-user-check"></i></button>
-      <button><i class="fa-solid fa-user-xmark"></i> </button>
+    <div class="btn">
+     <button><i class="fa-solid fa-user-check"></i></button>
+     <button><i class="fa-solid fa-user-xmark"></i> </button>
+    </div>
+   </div>
+   <div class="box">
+    <div class="min-box">
+     <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
+     <div class="friendName">
+      <h3>user name</h3>
+      <p><span>5</span>mutual friends</p>
      </div>
     </div>
-    <div class="box">
-     <div class="min-box">
-      <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
-      <div class="friendName">
-       <h3>user name</h3>
-       <p><span>5</span>mutual friends</p>
-      </div>
-     </div>
-     <div class="btn">
-      <button><i class="fa-solid fa-user-check"></i></button>
-      <button><i class="fa-solid fa-user-xmark"></i> </button>
+    <div class="btn">
+     <button><i class="fa-solid fa-user-check"></i></button>
+     <button><i class="fa-solid fa-user-xmark"></i> </button>
+    </div>
+   </div>
+   <div class="box">
+    <div class="min-box">
+     <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
+     <div class="friendName">
+      <h3>user name</h3>
+      <p><span>5</span>mutual friends</p>
      </div>
     </div>
-    <div class="box">
-     <div class="min-box">
-      <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
-      <div class="friendName">
-       <h3>user name</h3>
-       <p><span>5</span>mutual friends</p>
-      </div>
-     </div>
-     <div class="btn">
-      <button><i class="fa-solid fa-user-check"></i></button>
-      <button><i class="fa-solid fa-user-xmark"></i> </button>
+    <div class="btn">
+     <button><i class="fa-solid fa-user-check"></i></button>
+     <button><i class="fa-solid fa-user-xmark"></i> </button>
+    </div>
+   </div>
+   <div class="box">
+    <div class="min-box">
+     <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
+     <div class="friendName">
+      <h3>user name</h3>
+      <p><span>5</span>mutual friends</p>
      </div>
     </div>
-    <div class="box">
-     <div class="min-box">
-      <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
-      <div class="friendName">
-       <h3>user name</h3>
-       <p><span>5</span>mutual friends</p>
-      </div>
-     </div>
-     <div class="btn">
-      <button><i class="fa-solid fa-user-check"></i></button>
-      <button><i class="fa-solid fa-user-xmark"></i> </button>
+    <div class="btn">
+     <button><i class="fa-solid fa-user-check"></i></button>
+     <button><i class="fa-solid fa-user-xmark"></i> </button>
+    </div>
+   </div>
+   <div class="box">
+    <div class="min-box">
+     <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
+     <div class="friendName">
+      <h3>user name</h3>
+      <p><span>5</span>mutual friends</p>
      </div>
     </div>
-    <div class="box">
-     <div class="min-box">
-      <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
-      <div class="friendName">
-       <h3>user name</h3>
-       <p><span>5</span>mutual friends</p>
-      </div>
-     </div>
-     <div class="btn">
-      <button><i class="fa-solid fa-user-check"></i></button>
-      <button><i class="fa-solid fa-user-xmark"></i> </button>
+    <div class="btn">
+     <button><i class="fa-solid fa-user-check"></i></button>
+     <button><i class="fa-solid fa-user-xmark"></i> </button>
+    </div>
+   </div>
+   <div class="box">
+    <div class="min-box">
+     <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
+     <div class="friendName">
+      <h3>user name</h3>
+      <p><span>5</span>mutual friends</p>
      </div>
     </div>
-    <div class="box">
-     <div class="min-box">
-      <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
-      <div class="friendName">
-       <h3>user name</h3>
-       <p><span>5</span>mutual friends</p>
-      </div>
-     </div>
-     <div class="btn">
-      <button><i class="fa-solid fa-user-check"></i></button>
-      <button><i class="fa-solid fa-user-xmark"></i> </button>
+    <div class="btn">
+     <button><i class="fa-solid fa-user-check"></i></button>
+     <button><i class="fa-solid fa-user-xmark"></i> </button>
+    </div>
+   </div>
+   <div class="box">
+    <div class="min-box">
+     <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
+     <div class="friendName">
+      <h3>user name</h3>
+      <p><span>5</span>mutual friends</p>
      </div>
     </div>
-    <div class="box">
-     <div class="min-box">
-      <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
-      <div class="friendName">
-       <h3>user name</h3>
-       <p><span>5</span>mutual friends</p>
-      </div>
+    <div class="btn">
+     <button><i class="fa-solid fa-user-check"></i></button>
+     <button><i class="fa-solid fa-user-xmark"></i> </button>
+    </div>
+   </div>
+   <div class="box">
+    <div class="min-box">
+     <img src="images/cat-1.jpg" alt="" width="130px" height="130px">
+     <div class="friendName">
+      <h3>user name</h3>
+      <p><span>5</span>mutual friends</p>
      </div>
-     <div class="btn">
-      <button><i class="fa-solid fa-user-check"></i></button>
-      <button><i class="fa-solid fa-user-xmark"></i> </button>
-     </div>
+    </div>
+    <div class="btn">
+     <button><i class="fa-solid fa-user-check"></i></button>
+     <button><i class="fa-solid fa-user-xmark"></i> </button>
     </div>
    </div>
   </div>
  </div>
-</body>
-
-</html>
+</div>
