2ef0e8c12801d23b3182226bac897be0ec91dfd0
nello8-0 <nour20900@gmail.com>
2022-05-24 04:48:04 +0200

    comment page

---
+++ comments.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="css/all.min.css">
+ <link rel="stylesheet" href="css/normalize.css">
+ <link rel="stylesheet" href="css/general.css">
+ <link rel="icon" href="images/bear.svg">
+ <link rel="stylesheet" href="css/comments.css">
+ <title>Concat | Comments</title>
+</head>
+
+<body>
+ <div class="comments-container">
+  <div class="choose">
+   <a href="resource.php">Back</a>
+   <a href="index.php">Home</a>
+   <a href="courses.php">Courses</a>
+   <a href="profile.php">Profile</a>
+   <a href="#">Refresh</a>
+  </div>
+  <div class="comments">
+   <div class="commet-input">
+    <textarea name="text" class="text" placeholder="Leave your comment her..."></textarea>
+    <div class="buttons">
+     <button class="post" id="com_cancle">Cancle</button>
+     <button class="post" id="com_post">Post</button>
+
+    </div>
+   </div>
+   <div class="comment-container">
+    <div class="comment">
+     <div class="comm">
+      <img src="user_media/image/bear.jpg" alt="profile picture">
+      <div class="box">
+       <h2>Ibrahim Azazy</h2>
+       <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A consequuntur vitae consectetur distinctio placeat
+        vero aspernatur illo molestiae nobis veniam qui nostrum fuga quisquam non labore magni, eaque laboriosam sunt!
+       </p>
+       <div class="effect">
+        <div class="btn">
+         <button class="reply">reply</button>
+         <button class="edit">edit</button>
+         <button class="del">delete</button>
+        </div>
+        <div class="text">
+         <textarea name="text" placeholder="Type your reoly..."></textarea>
+         <div class="buttons">
+          <button class="post" id="post">Post</button>
+          <button class="post" id="cancle">Cancle</button>
+         </div>
+        </div>
+       </div>
+
+      </div>
+     </div>
+     <div class="repl">
+      <img src="user_media/image/bear.jpg" alt="profile picture">
+      <div class="box">
+       <h2>Ibrahim Azazy</h2>
+       <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A consequuntur vitae consectetur distinctio placeat
+        vero aspernatur illo molestiae nobis veniam qui nostrum fuga quisquam non labore magni, eaque laboriosam sunt!
+       </p>
+       <div class="effect">
+        <div class="btn">
+         <button class="reply">reply</button>
+         <button class="edit">edit</button>
+         <button class="del">delete</button>
+        </div>
+        <div class="text">
+         <textarea name="text" placeholder="Type your reoly..."></textarea>
+         <div class="buttons">
+          <button class="post" id="post">Post</button>
+          <button class="post" id="cancle">Cancle</button>
+         </div>
+        </div>
+       </div>
+
+      </div>
+     </div>
+    </div>
+
+    <div class="comment">
+     <div class="comm">
+      <img src="user_media/image/bear.jpg" alt="profile picture">
+      <div class="box">
+       <h2>Ibrahim Azazy</h2>
+       <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A consequuntur vitae consectetur distinctio placeat
+        vero aspernatur illo molestiae nobis veniam qui nostrum fuga quisquam non labore magni, eaque laboriosam sunt!
+       </p>
+       <div class="effect">
+        <div class="btn">
+         <button class="reply">reply</button>
+         <button class="edit">edit</button>
+         <button class="del">delete</button>
+        </div>
+        <div class="text">
+         <textarea name="text" placeholder="Type your reoly..."></textarea>
+         <div class="buttons">
+          <button class="post" id="post">Post</button>
+          <button class="post" id="cancle">Cancle</button>
+         </div>
+        </div>
+       </div>
+
+      </div>
+     </div>
+    </div>
+    <div class="comment">
+     <div class="comm">
+      <img src="user_media/image/bear.jpg" alt="profile picture">
+      <div class="box">
+       <h2>Ibrahim Azazy</h2>
+       <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A consequuntur vitae consectetur distinctio placeat
+        vero aspernatur illo molestiae nobis veniam qui nostrum fuga quisquam non labore magni, eaque laboriosam sunt!
+       </p>
+       <div class="effect">
+        <div class="btn">
+         <button class="reply">reply</button>
+         <button class="edit">edit</button>
+         <button class="del">delete</button>
+        </div>
+        <div class="text">
+         <textarea name="text" placeholder="Type your reoly..."></textarea>
+         <div class="buttons">
+          <button class="post" id="post">Post</button>
+          <button class="post" id="cancle">Cancle</button>
+         </div>
+        </div>
+       </div>
+
+      </div>
+     </div>
+     <div class="repl">
+      <img src="user_media/image/bear.jpg" alt="profile picture">
+      <div class="box">
+       <h2>Ibrahim Azazy</h2>
+       <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. A consequuntur vitae consectetur distinctio placeat
+        vero aspernatur illo molestiae nobis veniam qui nostrum fuga quisquam non labore magni, eaque laboriosam sunt!
+       </p>
+       <div class="effect">
+        <div class="btn">
+         <button class="reply">reply</button>
+         <button class="edit">edit</button>
+         <button class="del">delete</button>
+        </div>
+        <div class="text">
+         <textarea name="text" placeholder="Type your reoly..."></textarea>
+         <div class="buttons">
+          <button class="post" id="post">Post</button>
+          <button class="post" id="cancle">Cancle</button>
+         </div>
+        </div>
+       </div>
+
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+
+ </div>
+</body>
+
+</html>
