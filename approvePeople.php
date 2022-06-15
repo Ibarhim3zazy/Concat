6f1319063691a17a447fcc0892d2465610a69b43
nello8-0 <nour20900@gmail.com>
2022-06-15 03:30:25 +0200

    approve posts

---
+++ approvePeople.php
@@ -0,0 +1,107 @@
+<link rel="stylesheet" href="css/approvePeople.css">
+
+<?php
+require 'header.php'
+ ?>
+
+<div class="approvePeople-container">
+  <div class="container">
+    <ul class="btns">
+      <a href="http://localhost/Concat/groupName.php?group_id=916430527114" class="group">
+        <i class="fa-solid fa-users"></i>
+      </a>
+      <a href="pinPost.php">
+        <i class="fa-solid fa-thumbtack"></i>
+      </a>
+      <a href="index.php">
+        <i class="fa-solid fa-house-user"></i>
+      </a>
+      <a href="#"><i class="fa-solid fa-angles-up"></i></a>
+      <a href=""><i class="fa-solid fa-rotate"></i></a>
+    </ul>
+    <h3>Pending . <span>7</span> </h3>
+    <button > Approve All </button>
+    <div class="box-container">
+    <div class="box">
+      <div class="cover">
+        <img src="images\backsearch7.jpg" alt="">
+      </div>
+      <img src="images\picsearch5.jpg" alt="">
+      <h3>Nereeman Samy</h3>
+      <div class="btn">
+        <input type="button" value="Approve">
+        <input type="button"  value="Delete">
+      </div>
+    </div>
+    <div class="box">
+      <div class="cover">
+        <img src="images\backsearch6.jpg" alt="">
+      </div>
+      <img src="images\picsearch4.jpg" alt="">
+      <h3>selena
+
+
+         Samy</h3>
+      <div class="btn">
+        <input type="button" value="Approve">
+        <input type="button"  value="Delete">
+      </div>
+    </div>
+    <div class="box">
+      <div class="cover">
+        <img src="images\backsearch1.jpg" alt="">
+      </div>
+      <img src="images\picsearch1.jpg" alt="">
+      <h3>kawser Samir</h3>
+      <div class="btn">
+        <input type="button" value="Approve">
+        <input type="button"  value="Delete">
+      </div>
+    </div>
+    <div class="box">
+      <div class="cover">
+        <img src="images\backsearch3.jpg" alt="">
+      </div>
+      <img src="images\picsearch3.jpg" alt="">
+      <h3>nella nore</h3>
+      <div class="btn">
+        <input type="button" value="Approve">
+        <input type="button"  value="Delete">
+      </div>
+    </div>
+    <div class="box">
+      <div class="cover">
+        <img src="images\backsearch5.jpg" alt="">
+      </div>
+      <img src="images\picsearch5.jpg" alt="">
+      <h3>aman zeyad</h3>
+      <div class="btn">
+        <input type="button" value="Approve">
+        <input type="button"  value="Delete">
+      </div>
+    </div>
+    <div class="box">
+      <div class="cover">
+        <img src="images\backsearch6.jpg" alt="">
+      </div>
+      <img src="images\picsearch6.jpg" alt="">
+      <h3>nevertiety Samy</h3>
+      <div class="btn">
+        <input type="button" value="Approve">
+        <input type="button"  value="Delete">
+      </div>
+    </div>
+    <div class="box">
+      <div class="cover">
+        <img src="images\backsearch8.jpg" alt="">
+      </div>
+      <img src="images\picsearch8.jpg" alt="">
+      <h3>ahmed Samy</h3>
+      <div class="btn">
+        <input type="button" value="Approve">
+        <input type="button"  value="Delete">
+      </div>
+    </div>
+  </div>
+  </div>
+</div>
