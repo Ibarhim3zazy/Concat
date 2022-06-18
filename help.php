9f6fd2d39dc7569e70fa0d3a709f2a11ed1dd568
nello8-0 <nour20900@gmail.com>
2022-06-18 06:31:42 +0200

    help page done

---
+++ help.php
@@ -0,0 +1,63 @@
+<link rel="stylesheet" href="css/help.css">
+
+<?php require_once 'header.php'; ?>
+
+<div class="help-container">
+  <div class="min-box">
+    <h2>Nour Hann</h2>
+    <p>
+      What can we help you find?
+    </p>
+    <div class="search">
+      <input type="search" placeholder="Search">
+      <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+    </div>
+  </div>
+  <div class="container">
+    <h2>Popular Topics</h2>
+    <div class="box-container">
+      <a class="box-1" href="">
+        <img src="images/profile.svg" alt="">
+        <h4>Account setting</h4>
+        <p>
+          Adjust setting, manage notification, learn about name change and more.
+        </p>
+      </a>
+      <a href="" class="box-1">
+        <img src="images/passwordd.svg" alt="">
+        <h4>Login and Password</h4>
+        <p>
+          Fix login issues and learn how to change or reset your Password.
+        </p>
+      </a>
+      <a href="" class="box-1">
+        <img src="images/safe.svg" alt="">
+        <h4>Privacy and Security</h4>
+        <p>
+          control who can see what you share and you add extra protection  to your account.
+        </p>
+      </a>
+      <a href="" class="box-1">
+        <img src="images/group-help.svg" alt="">
+        <h4>Groups</h4>
+        <p>
+          How to create, manage, and use groups.
+        </p>
+      </a>
+      <a href="" class="box-1">
+        <img src="images/course.svg" alt="">
+        <h4>Couses</h4>
+        <p>
+          How to manage, follow groups.
+        </p>
+      </a>
+      <a href="" class="box-1">
+        <img src="images/email.svg" alt="">
+        <h4>Confirm Email</h4>
+        <p>
+          How to change, reset , add new Email.
+        </p>
+      </a>
+    </div>
+  </div>
+</div>
